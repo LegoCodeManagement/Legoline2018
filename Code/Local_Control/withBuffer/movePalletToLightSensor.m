54e201813d34741567ebca217924c856b974ce83
Harry Rendell <iCade@dhcp-10-248-156-138.eduroam.wireless.private.cam.ac.uk>
2018-07-16 10:19:44 +0100

    moving files

---
+++ Code/Local_Control/withBuffer/movePalletToLightSensor.m
@@ -0,0 +1,21 @@
+function result = movePalletToLightSensor(motor, power, nxt, port, currentValue, timeOut)
+%movePalletToLightSensor(motor, power, nxt, port, currentValue)
+%Move the pallet until the light sensor detects it
+%Return true if the function is completed within timeOut false otherwise
+tic;
+currentTime = toc;
+movePallet = NXTMotor(motor, 'Power', power);
+movePallet.SpeedRegulation = 0;
+movePallet.SendToNXT(nxt);
+while abs(GetLight(port, nxt) - currentValue) < 11
+    if (toc - currentTime > timeOut)
+        disp('The light sensor hasnt detected the pallet before timeout');
+        movePallet.Stop('off', nxt);
+        result = false;
+        return;
+    end
+end
+result = true;
+pause(0.7)
+movePallet.Stop('off', nxt);
+end
