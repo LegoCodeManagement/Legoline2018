18963cccaff46108ab94062f268626cfcfe3a983
harry-rendell <harryrendell9@gmail.com>
2018-07-16 16:59:39 +0100

    adding file path and moving RWTH

--- Code/Local_Control/withBuffer/BUFFERupstream.m
+++ Code/Local_Control/withBuffer/BUFFERupstream.m
@@ -1,4 +1,4 @@
-COM_CloseNXT('all')
+addpath RWTHMindstormsNXT;
 
 %open config file and save variable names and values column 1 and 2
 %respectively.
@@ -9,15 +9,15 @@ fclose(config);
 power = str2double(out{2}(strcmp('SPEED_U',out{1})));
 Uaddr = char(out{2}(strcmp('Upstream',out{1})));
 
-U = COM_OpenNXTEx('USB', Uaddr);
+nxtU = COM_OpenNXTEx('USB', Uaddr);
 
 
 fstatus = memmapfile('status.txt', 'Writable', true, 'Format', 'int8');
 j1 = memmapfile('Junction1.txt','Writable',true);
 disp(j1.Data(1));
 
-OpenLight(SENSOR_2,'ACTIVE',U)
-OpenSwitch(SENSOR_1,U)
+OpenLight(SENSOR_2,'ACTIVE',nxtU)
+OpenSwitch(SENSOR_1,nxtU)
 
 
 disp('UPSTREAM');
@@ -26,7 +26,7 @@ disp('waiting for ready signal')
 while fstatus.Data(1) == 48
     pause(0.1);
 end
-currentValueU = GetLight(SENSOR_2,U);
+currentValueU = GetLight(SENSOR_2,nxtU);
 
 palletHasLeft = [timer('TimerFcn','j1.Data(1) = j1.Data(1) - 1','StartDelay',3); 
                  timer('TimerFcn','j1.Data(1) = j1.Data(1) - 1','StartDelay',3);
@@ -47,13 +47,13 @@ while (j<6) && (fstatus.Data(1) == 49)
 		j1.Data(1) = j1.Data(1) + 1;
 		clear toc
 		tic
-		feedPallet(U,SENSOR_1,MOTOR_A);
+		feedPallet(nxtU,SENSOR_1,MOTOR_A);
 		j=j+1;
         if fstatus.Data(1) ~= 49
             break
             disp('break');
         end
-		movePalletToLightSensor(MOTOR_B,power,U,SENSOR_2,currentValueU,20);
+		movePalletToLightSensor(MOTOR_B,power,nxtU,SENSOR_2,currentValueU,20);
 		start(palletHasLeft(j));
 	end
 	pause(0.25)
@@ -62,6 +62,6 @@ end
 delete(timerfind);
 disp('Upstream STOPPED')
 clear j1;
-CloseSensor(SENSOR_1, nxtF1);
-CloseSensor(SENSOR_2, nxtF1);
+CloseSensor(SENSOR_1, nxtU);
+CloseSensor(SENSOR_2, nxtU);
 COM_CloseNXT(nxtU);
--- Code/Local_Control/withBuffer/master.m
+++ Code/Local_Control/withBuffer/master.m
@@ -1,3 +1,5 @@
+addpath RWTHMindstormsNXT;
+pwd
 COM_CloseNXT('all');
 
 %Transfer Line 1 is Okay now do not change  TEST_Feed1 and TEST_Transfer1
@@ -9,16 +11,15 @@ COM_CloseNXT('all');
 
 run initialise
 
+%!matlab  -nodesktop -minimize -nosplash -r BUFFERfeed1&
+%!matlab  -nodesktop -minimize -nosplash -r BUFFERtransfer1&
+%!matlab  -nodesktop -minimize -nosplash -r BUFFERmainline1&
+!matlab  -nodesktop -minimize -nosplash -r BUFFERupstream&
+
 fstatus = memmapfile('status.txt', 'Writable', true, 'Format', 'int8');
 fstatus.Data(1) = 48;
 input('press ENTER to start Legoline');
 fstatus.Data(1) = 49;
 input('press ENTER to stop Legoline');
 fstatus.Data(1) = 50;
-
-%!matlab  -nodesktop -minimize -nosplash -r BUFFERfeed1&
-%!matlab  -nodesktop -minimize -nosplash -r BUFFERtransfer1&
-%!matlab  -nodesktop -minimize -nosplash -r BUFFERmainline1&
-!matlab  -nodesktop -minimize -nosplash -r BUFFERupstream&
-
 
