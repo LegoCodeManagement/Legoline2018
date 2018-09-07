f7e649a0baeaa299740dda661e2eb2f83d883c37
Harry Rendell <iCade@dhcp-10-240-159-26.cp.wireless.private.cam.ac.uk>
2018-09-07 10:31:38 +0100

    testing conf write

--- Code/testwritechar.m
+++ Code/testwritechar.m
@@ -1,13 +1,10 @@
 config  = fopen('config.txt','rt');
 out 	= textscan(config, '%s %s');
 fclose(config);
-C = [char(out{1}),char(out{2})];
-format = '%s " " %s\n';
+C = [string(out{1}),string(out{2})];
+format = '%s %s\n';
 config2 = fopen('config2.txt','w');
 for i=1:1:length(C)
-    for j=1:1:length(C(1,:))
-    fprintf(config2,'%c',C(i,j));
-    end
-    
+    fprintf(config2,format,C(i,:));
 end
 fclose(config2);
